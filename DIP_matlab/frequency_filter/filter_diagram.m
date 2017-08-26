40f59c320e31ef6b594ea7bacb514dcec434acf9
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2017-08-26 11:05:12 +0800

    新增同态滤波器

--- DIP_matlab/frequency_filter/filter_diagram.m
+++ DIP_matlab/frequency_filter/filter_diagram.m
@@ -1,2 +1,78 @@
 clc;
 clear all;
+
+height = 512;width = 512;
+center_height = round(height / 2);
+center_width = round(width / 2);
+cut_frequency1 = 128;
+cut_frequency2 = 192;
+
+H = zeros(height, width);
+
+%% 理想低高通滤波器
+fig_1 = figure(1);
+set(fig_1, 'name', 'Fig1 : Ideal Filter', 'Numbertitle', 'off');
+
+for h = 1:height
+    for w = 1:width
+        dis = sqrt(h^2 + w^2);
+        if dis <= cut_frequency1
+            H(h, w) = 1;
+        else
+            H(h, w) = 0;
+        end
+    end
+end
+subplot(221);
+imshow(H);
+title('理想低通滤波器二维图示');
+xlabel('width');ylabel('height');
+
+for h = 1:height
+    for w = 1:width
+        dis = sqrt(h^2 + w^2);
+        if dis >= cut_frequency1
+            H(h, w) = 1;
+        else
+            H(h, w) = 0;
+        end
+    end
+end
+subplot(222);
+imshow(H);
+title('理想高通滤波器二维图示');
+xlabel('width');ylabel('height');
+
+%% 巴特沃斯低高通滤波器
+fig_2 = figure(2);
+set(fig_2, 'name', 'Fig2 : Butterworth Filter', 'Numbertitle', 'off');
+
+for h = 1:height
+    for w = 1:width
+        dis = sqrt(h^2 + w^2);
+        if dis <= cut_frequency1
+            H(h, w) = 1;
+        else
+            H(h, w) = 0;
+        end
+    end
+end
+subplot(221);
+imshow(H);
+title('理想低通滤波器二维图示');
+xlabel('width');ylabel('height');
+
+for h = 1:height
+    for w = 1:width
+        dis = sqrt(h^2 + w^2);
+        if dis >= cut_frequency1
+            H(h, w) = 1;
+        else
+            H(h, w) = 0;
+        end
+    end
+end
+subplot(222);
+imshow(H);
+title('理想高通滤波器二维图示');
+xlabel('width');ylabel('height');
--- DIP_matlab/frequency_filter/homomorphic_ideal_highpass.m
+++ DIP_matlab/frequency_filter/homomorphic_ideal_highpass.m
@@ -1,10 +1,12 @@
-%% ideal lowpass
-function image = ideal_lowpass(image_origin, cut_frequency)
+% 同态滤波器(理想高通)
+function image = homomorphic_ideal_highpass(image_origin, cut_frequency)
 
 [height, width] = size(image_origin);                       % calculate the height and width of image
 H = zeros(height, width);                                   % initial filter H
-image_origin_fft = fft2(image_origin);                      % fft
-image_origin_fftshift = fftshift(image_origin_fft);         % fftshift, move the low frequency to the center
+image_origin_log = log(double(image_origin));               % log
+image_origin_log_fft = fft2(image_origin_log);              % fft
+
+image_origin_log_fftshift = fftshift(image_origin_log_fft); % fftshift, move the low frequency to the center
 center_height = round(height / 2);                          % center point(height)
 center_width = round(width / 2);                            % center point(width)
 max_frequency = ...
@@ -17,13 +19,15 @@ end
 for h = 1:height
     for w = 1:width
         dis = sqrt((h - center_height)^2 + (w - center_width)^2);
-        if dis <= cut_frequency
+        if dis >= cut_frequency
             H(h, w) = 1;
         end
     end
 end
 
-image_fftshift = image_origin_fftshift .* H;
-image_fft = ifftshift(image_fftshift);
-image = ifft2(image_fft);
+image_log_fftshift = image_origin_log_fftshift .* H;
+image_log_fft = ifftshift(image_log_fftshift);
+image_log = ifft2(image_log_fft);
+image = exp(image_log);
 image = uint8(real(image));
+
